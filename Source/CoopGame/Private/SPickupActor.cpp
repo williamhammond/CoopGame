ad848046fa3881ab3a7bec9fd027cd6d72ff8e37
William Hammond <william.t.hammond@gmail.com>
2022-01-23 11:37:04 -0500

    Add basic pickup actor

---
+++ Source/CoopGame/Private/SPickupActor.cpp
@@ -0,0 +1,27 @@
+﻿#include "SPickupActor.h"
+
+#include "Components/DecalComponent.h"
+#include "Components/SphereComponent.h"
+
+
+ASPickupActor::ASPickupActor()
+{
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent"));
+	SphereComponent->SetSphereRadius(75.0f);
+	RootComponent = SphereComponent;
+
+	DecalComponent = CreateDefaultSubobject<UDecalComponent>(TEXT("DecalComponent"));
+	DecalComponent->SetRelativeRotation(FRotator(90, 0.0f, 0.0f));
+	DecalComponent->DecalSize = FVector(64, 75, 75);
+	DecalComponent->SetupAttachment(SphereComponent);
+}
+
+void ASPickupActor::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ASPickupActor::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+}
---
+++ Source/CoopGame/Public/SPickupActor.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SPickupActor.generated.h"
+
+class USphereComponent;
+class UDecalComponent;
+
+UCLASS()
+class COOPGAME_API ASPickupActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ASPickupActor();
+
+protected:
+	virtual void BeginPlay() override;
+	UPROPERTY(VisibleAnywhere, Category="Componenets")
+	USphereComponent* SphereComponent;
+	
+	UPROPERTY(VisibleAnywhere, Category="Componenets")
+	UDecalComponent* DecalComponent;
+public:
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
+};
