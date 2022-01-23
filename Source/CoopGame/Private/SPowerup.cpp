2e0ca9ab0f44e3e5713a2dab1e4d417556b80617
William Hammond <william.t.hammond@gmail.com>
2022-01-23 11:59:19 -0500

    Adds basic powerup class

---
+++ Source/CoopGame/Private/SPowerup.cpp
@@ -0,0 +1,40 @@
+﻿#include "SPowerup.h"
+
+#include "TimerManager.h"
+
+
+ASPowerup::ASPowerup()
+{
+	PowerupInterval = 0.0f;
+	TotalNumberOfTicks = 0.0f;
+}
+
+void ASPowerup::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ASPowerup::OnTickPowerup()
+{
+	TickCount++;
+	OnPowerupTicked();
+
+	if (TickCount >= TotalNumberOfTicks)
+	{
+		OnExpired();
+		GetWorldTimerManager().ClearTimer(TimerHandle_PowerupTick);
+	}
+}
+
+void ASPowerup::ActivatePowerup()
+{
+	if (PowerupInterval > 0.0f)
+	{
+		GetWorldTimerManager().SetTimer(TimerHandle_PowerupTick, this, &ASPowerup::OnTickPowerup, PowerupInterval, true,
+		                                0.0f);
+	}
+	else
+	{
+		OnTickPowerup();
+	}
+}
---
+++ Source/CoopGame/Public/SPowerup.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SPowerup.generated.h"
+
+UCLASS()
+class COOPGAME_API ASPowerup : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ASPowerup();
+
+protected:
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Powerups")
+	float PowerupInterval;
+	
+	UPROPERTY(EditDefaultsOnly, Category="Powerups")
+	int32 TotalNumberOfTicks;
+
+	FTimerHandle TimerHandle_PowerupTick;
+	
+	UFUNCTION()
+	void OnTickPowerup();
+
+	int32 TickCount;
+
+	void ActivatePowerup();
+public:
+	UFUNCTION(BlueprintImplementableEvent, Category="Powerups")
+	void OnActivated();
+
+	UFUNCTION(BlueprintImplementableEvent, Category="Powerups")
+	void OnPowerupTicked();
+
+	UFUNCTION(BlueprintImplementableEvent, Category="Powerups")
+	void OnExpired();
+};
